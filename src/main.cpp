2f24507edebcafbc355e0bfbb81cb849e0cf27e5
Your Name <you@example.com>
2021-03-17 02:50:55 +0000

    main

---
+++ src/main.cpp
@@ -0,0 +1,93 @@
+#include <optional>
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <io2d.h>
+#include "route_model.h"
+#include "render.h"
+#include "route_planner.h"
+
+using namespace std::experimental;
+
+static std::optional<std::vector<std::byte>> ReadFile(const std::string &path)
+{   
+    std::ifstream is{path, std::ios::binary | std::ios::ate};
+    if( !is )
+        return std::nullopt;
+    
+    auto size = is.tellg();
+    std::vector<std::byte> contents(size);    
+    
+    is.seekg(0);
+    is.read((char*)contents.data(), size);
+
+    if( contents.empty() )
+        return std::nullopt;
+    return std::move(contents);
+}
+void getcoords(float in[2],std::string prompt){
+	while(true){
+      	std::cout<<prompt<<"(float,float): ";
+  		char temp;
+  		std::cin>>in[0]>>temp>>in[1];
+  		std::cout<<std::endl;
+      	if(in[0]<0||in[0]>100||in[1]<0||in[1]>100){
+          std::cerr<<"floats must be in range (1,100)"<<std::endl;
+          continue;
+        }
+      	break;
+    }
+}
+int main(int argc, const char **argv)
+{    
+    std::string osm_data_file = "";
+    if( argc > 1 ) {
+        for( int i = 1; i < argc; ++i )
+            if( std::string_view{argv[i]} == "-f" && ++i < argc )
+                osm_data_file = argv[i];
+    }
+    else {
+        std::cout << "To specify a map file use the following format: " << std::endl;
+        std::cout << "Usage: [executable] [-f filename.osm]" << std::endl;
+        osm_data_file = "../map.osm";
+    }
+    
+    std::vector<std::byte> osm_data;
+ 
+    if( osm_data.empty() && !osm_data_file.empty() ) {
+        std::cout << "Reading OpenStreetMap data from the following file: " <<  osm_data_file << std::endl;
+        auto data = ReadFile(osm_data_file);
+        if( !data )
+            std::cout << "Failed to read." << std::endl;
+        else
+            osm_data = std::move(*data);
+    }
+    
+    // TODO 1: Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
+    // user input for these values using std::cin. Pass the user input to the
+    // RoutePlanner object below in place of 10, 10, 90, 90.
+	float start[2],end[2];
+    getcoords(start,"Enter start coords");
+    getcoords(end,"Enter end coords");
+    // Build Model.
+    RouteModel model{osm_data};
+
+    // Create RoutePlanner object and perform A* search.
+    RoutePlanner route_planner{model, start[0], start[1], end[0], end[1]};
+    route_planner.AStarSearch();
+
+    std::cout << "Distance: " << route_planner.GetDistance() << " meters. \n";
+
+    // Render results of search.
+    Render render{model};
+
+    auto display = io2d::output_surface{400, 400, io2d::format::argb32, io2d::scaling::none, io2d::refresh_style::fixed, 30};
+    display.size_change_callback([](io2d::output_surface& surface){
+        surface.dimensions(surface.display_dimensions());
+    });
+    display.draw_callback([&](io2d::output_surface& surface){
+        render.Display(surface);
+    });
+    display.begin_show();
+}
